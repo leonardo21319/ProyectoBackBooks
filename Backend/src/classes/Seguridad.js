375dc85c337c9e0a2ecd229481ba2015a8dfb892
EmmanuelZc <103443577+EmmanuelZc@users.noreply.github.com>
2025-06-13 22:57:34 -0600

    Revert "configuracion cluster"
    This reverts commit 921179cca1469248f4ce2d4799b64880e1c355e9.

--- Backend/src/classes/Seguridad.js
+++ Backend/src/classes/Seguridad.js
@@ -1,14 +1,14 @@
-import bcrypt from "bcrypt";
-import dotenv from "dotenv";
-import crypto from "crypto";
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+import crypto from 'crypto';
 
 dotenv.config();
 
 export default class Seguridad {
-  static algorithm = "aes-256-cbc";
-  static key = Buffer.from(process.env.ENCRYPT_KEY, "hex");
-  static iv = Buffer.alloc(16, 0); // Un IV
-  static blacklist = new Map();
+  static  algorithm = 'aes-256-cbc';
+  static  key = Buffer.from(process.env.ENCRYPT_KEY, 'hex');
+  static  iv = Buffer.alloc(16, 0); // Un IV
+  static  blacklist = new Map(); 
 
   static async hash(contrasena) {
     return bcrypt.hash(contrasena, 10);
@@ -20,25 +20,25 @@ export default class Seguridad {
 
   static cifrar(textoplano) {
     const cipher = crypto.createCipheriv(algorithm, key, iv);
-    let encrypted = cipher.update(textoplano, "utf8", "hex");
-    encrypted += cipher.final("hex");
+    let encrypted = cipher.update(textoplano, 'utf8', 'hex');
+    encrypted += cipher.final('hex');
     return {
-      iv: iv.toString("hex"),
-      valor: encrypted,
+      iv: iv.toString('hex'),
+      valor: encrypted
     };
   }
-
+  
   static descifrar(valor) {
-    const decipher = crypto.createDecipheriv(algorithm, key, iv);
-    let decrypted = decipher.update(valor, "hex", "utf8");
-    decrypted += decipher.final("utf8");
+    const decipher = crypto.createDecipheriv(algorithm, key, iv); 
+    let decrypted = decipher.update(valor, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
     return decrypted;
   }
 
   static agregarTokenBlacklist(token) {
-    Seguridad.blacklist.set(token, Date.now());
+    Seguridad.blacklist.set(token, Date.now()); 
   }
-
+  
   static verificarTokenBlacklist(token) {
     return Seguridad.blacklist.has(token); // Verifica si el token está en la blacklist
   }
