921179cca1469248f4ce2d4799b64880e1c355e9
EmmanuelZc <103443577+EmmanuelZc@users.noreply.github.com>
2025-06-13 22:48:49 -0600

    configuracion cluster

--- Backend/src/config/configDB.js
+++ Backend/src/config/configDB.js
@@ -1,14 +1,24 @@
-import { config } from 'dotenv';
-import { Pool } from 'pg';
-
+import { config } from "dotenv";
+import { Pool } from "pg";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const pool = new Pool({
-  user: process.env.DB_USER,         
-  password: process.env.DB_PASS,     
-  database: process.env.DB_NAME,     
-  host: process.env.DB_HOST,         
-  port: process.env.DB_PORT,         
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+    ca: process.env.DB_SSL_CA
+      ? fs.readFileSync(path.join(__dirname, process.env.DB_SSL_CA)).toString()
+      : undefined,
+  },
 });
 
 export default pool;
